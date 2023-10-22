3267f1336b8384815e1c9984639cdfb7bf0da5f6
Hesham Moneer <hesham.moneer@oultook.com>
2023-10-22 13:44:04 +0300

    moved db name and db user to env file

--- DB/entrypoint.sh
+++ DB/entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-/opt/mssql-tools/bin/sqlcmd -S localhost -l 60 -U SA -P ${SA_PASSWORD} -i init.sql &&
+/opt/mssql-tools/bin/sqlcmd -S localhost -l 60 -U ${SA_USER} -P ${SA_PASSWORD} -v DB_NAME=${DB_NAME} -i init.sql &&
 
-/opt/mssql-tools/bin/sqlcmd -S localhost -l 60 -U SA -P ${SA_PASSWORD} -d LMS -i proc-book.sql &&
+/opt/mssql-tools/bin/sqlcmd -S localhost -l 60 -U ${SA_USER} -P ${SA_PASSWORD} -d ${DB_NAME} -i proc-book.sql &&
 
-/opt/mssql-tools/bin/sqlcmd -S localhost -l 60 -U SA -P ${SA_PASSWORD} -d LMS -i proc-borrow.sql &&
+/opt/mssql-tools/bin/sqlcmd -S localhost -l 60 -U ${SA_USER} -P ${SA_PASSWORD} -d ${DB_NAME} -i proc-borrow.sql &&
 
-/opt/mssql-tools/bin/sqlcmd -S localhost -l 60 -U SA -P ${SA_PASSWORD} -d LMS -i proc-borrower.sql &
+/opt/mssql-tools/bin/sqlcmd -S localhost -l 60 -U ${SA_USER} -P ${SA_PASSWORD} -d ${DB_NAME} -i proc-borrower.sql &
 
 /opt/mssql/bin/sqlservr
