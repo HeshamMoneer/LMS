47ba3f108d350a6903b7fb3ba8cbced7d642d9b2
Hesham Moneer <hesham.moneer@oultook.com>
2023-10-22 13:28:41 +0300

    Moved DB password to an env file

--- DB/entrypoint.sh
+++ DB/entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-/opt/mssql-tools/bin/sqlcmd -S localhost -l 60 -U SA -P 'y5#kA!Lm45]Uj*<' -i init.sql &&
+/opt/mssql-tools/bin/sqlcmd -S localhost -l 60 -U SA -P ${SA_PASSWORD} -i init.sql &&
 
-/opt/mssql-tools/bin/sqlcmd -S localhost -l 60 -U SA -P 'y5#kA!Lm45]Uj*<' -d LMS -i proc-book.sql &&
+/opt/mssql-tools/bin/sqlcmd -S localhost -l 60 -U SA -P ${SA_PASSWORD} -d LMS -i proc-book.sql &&
 
-/opt/mssql-tools/bin/sqlcmd -S localhost -l 60 -U SA -P 'y5#kA!Lm45]Uj*<' -d LMS -i proc-borrow.sql &&
+/opt/mssql-tools/bin/sqlcmd -S localhost -l 60 -U SA -P ${SA_PASSWORD} -d LMS -i proc-borrow.sql &&
 
-/opt/mssql-tools/bin/sqlcmd -S localhost -l 60 -U SA -P 'y5#kA!Lm45]Uj*<' -d LMS -i proc-borrower.sql &
+/opt/mssql-tools/bin/sqlcmd -S localhost -l 60 -U SA -P ${SA_PASSWORD} -d LMS -i proc-borrower.sql &
 
 /opt/mssql/bin/sqlservr
