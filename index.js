638b71af6c902a74185f56ee5d5b67b8b1b6af49
Hesham Moneer <hesham.moneer@oultook.com>
2023-10-22 18:16:45 +0300

    Created backend skeleton

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const yml = require('yamljs');
+const app = express();
+const port = process.env.BACKEND_PORT;
+
+
+const swaggerYML = yml.load('./swagger.yml');
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerYML));
+
+
+const bookRoutes = require('./API/book_api');
+const borrowRoutes = require('./API/borrow_api');
+const borrowerRoutes = require('./API/borrower_api');
+
+app.use('/book', bookRoutes);
+app.use('/borrow', borrowRoutes);
+app.use('/borrower', borrowerRoutes);
+
+app.listen(port, () => {
+    console.log(`Server is listening at http://localhost:${port}`);
+});
